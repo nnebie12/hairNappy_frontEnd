5aeccd94a9476206ece667c00dd20c1f072c295b
kassinnebie <nnebiekassi12@gmail.com>
2024-05-31 13:13:07 +0200

    mise en place de la page d'acceuil

--- js/login.js
+++ js/login.js
@@ -1,33 +1,34 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+            event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const formObject = {};
-    formData.forEach((value, key) => {
-        formObject[key] = value;
-    });
+            const formData = new FormData(event.target);
+            const formObject = {};
+            formData.forEach((value, key) => {
+                formObject[key] = value;
+            });
 
-    fetch('http://localhost:8000/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formObject),
-    })
-    .then(response => {
-        if (!response.ok) {
-            return response.text().then(text => { throw new Error(text) });
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.status === 'success') {
-            alert('Connexion réussie!');
-        } else {
-            alert('Erreur de connexion: ' + (data.error || ''));
-        }
-    })
-    .catch(error => {
-        alert('Erreur de connexion: ' + error.message);
-    });
-});
+                
+                fetch('http://localhost:8000/api/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formObject),
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        return response.text().then(text => { throw new Error(text) });
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data.status === 'success') {
+                        alert('Connexion réussie!');
+                    } else {
+                        alert('Erreur de connexion: ' + (data.error || ''));
+                    }
+                })
+                .catch(error => {
+                    alert('Erreur de connexion: ' + error.message);
+                });
+        });
